8e2edad245f4a5185b6ff5d8becb5be28568f49b
Mike Lofgren <mikeylofgren@gmail.com>
2020-01-28 22:30:03 -0800

    setup React Navigation.

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import IndexScreen from "./src/screens/IndexScreen";
+//Create Navigator. It takes in two arguments. First one is route configuration
+// object which lists out all of the possible screens a user can navigate to.
+//The second one is
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen
   },
-});
+  {
+    //Second argument is configuration options, specifically for Stack Navigator.
+    //Initial Route Name is for when the app spins up.
+    initialRouteName: "Index",
+    //Default Navigation Options sets the title inside of the header.
+    defaultNavigationOptions: {
+      title: "Entries"
+    }
+  }
+);
+//Passing in Stack Navigator to Create App Container
+export default createAppContainer(navigator);
