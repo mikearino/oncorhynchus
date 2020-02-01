4530834f7d4775dc7a130e780bc9094aec196946
Mike Lofgren <mikeylofgren@gmail.com>
2020-01-31 20:49:29 -0800

    setup reducer.

--- src/context/EntryContext.js
+++ src/context/EntryContext.js
@@ -1,22 +1,31 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 //This is the pipe, when created it comes with provider.
 //Whatever information we provide it is going to become
 //available to all of our child components
 const EntryContext = React.createContext();
+//React knows to run reducer when dispatch is called
+const entryReducer = (state, action) => {
+  switch (action.type) {
+    case "add_entry":
+      //return new array with all current values of state and add new entry
+      return [...state, { title: `Entry #${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
 
 //create a component that can accept another component as an argument
 export const EntryProvider = ({ children }) => {
-  //entries=initial State, setEntries is the setter, get value from use
-  //state
-  const [entries, setEntries] = useState([]);
-  //add a new array, then take all entries and add to new array
-  //then add new object into array
-  const addEntries = () => {
-    setEntries([...entries, { title: `Entry #${entries.length + 1}` }]);
+  //1st arg= reducer to use, 2nd is initial state object
+  const [entries, dispatch] = useReducer(blogReducer, []);
+  //method to run dispatch that gets passed down to provider
+  const addEntry = () => {
+    dispatch({ type: "add_entry" });
   };
+
   return (
-    <EntryContext.Provider value={{ data: entries, addEntries }}>
+    <EntryContext.Provider value={{ data: entries, addEntry }}>
       {/* argument is going to be shown inside EntryProvider */}
       {children}
     </EntryContext.Provider>
