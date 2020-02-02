a64c63998570fe0f0fed7bf16350c2ef7d28055f
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-01 23:18:34 -0800

    importing context, useState.

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,7 +1,16 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState, useContext } from "react";
+import { Text, View, StyleSheet, TextInput } from "react-native";
+import Context from "../context/EntryContext";
+import { useState } from "react";
 
 const EditScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+  //Find the entry and compoare it with the id of navigation.getParam,
+  //then bind
+  const entry = state.find(entry === navigation.getParam("id"));
+  //Initial value for title/state is going to be ^ this entries' title.
+  const [title, setTitle] = useState(entry.title);
+  const [content, setContetn] = useState(entry.content);
   return (
     <View>
       <Text>Edit Screen - {navigation.getParam("id")}</Text>
