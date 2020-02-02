d64b79d06ee104dd4a5b86b8b9a96eb249eac5ea
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-01 22:45:57 -0800

    hook up Edit Screen.

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const EditScreen = () => {
+  return (
+    <View>
+      <Text>This is the Edit Screen</Text>
+    </View>
+  );
+};
+
+export default EditScreen;
