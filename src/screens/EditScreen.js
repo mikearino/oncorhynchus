6fa556952dcee1a6fb7b298d4b6ddf425746f536
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-01 23:40:32 -0800

    change .find syntac in edit screen.

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,19 +1,19 @@
 import React, { useState, useContext } from "react";
 import { Text, View, StyleSheet, TextInput } from "react-native";
-import Context from "../context/EntryContext";
-import { useState } from "react";
+import { Context } from "../context/EntryContext";
 
 const EditScreen = ({ navigation }) => {
   const { state } = useContext(Context);
   //Find the entry and compoare it with the id of navigation.getParam,
   //then bind
-  const entry = state.find(entry === navigation.getParam("id"));
+  const entry = state.find(entry => entry.id === navigation.getParam("id"));
   //Initial value for title/state is going to be ^ this entries' title.
   const [title, setTitle] = useState(entry.title);
-  const [content, setContetn] = useState(entry.content);
+  // const [content, setContent] = useState(entry.content);
   return (
     <View>
-      <Text>Edit Screen - {navigation.getParam("id")}</Text>
+      <Text>Edit Title:</Text>
+      <TextInput value={title} onChangeText={newTitle => setTitle(newTitle)} />
     </View>
   );
 };
