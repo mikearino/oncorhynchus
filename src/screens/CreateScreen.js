036a8822e9c6ad3114ca9cb218a1a16066f3bb09
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-01 21:47:34 -0800

    hook up local state to input forms.

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,14 +1,46 @@
-import React, { useContext } from "react";
-import { View, Text, Stylesheet } from "react-native";
+import React, { useContext, useState } from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import { HeaderTitle } from "react-navigation-stack";
 import { Context } from "../context/EntryContext";
 
 const CreateScreen = ({ navigation }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   return (
     <View>
-      <Text>create</Text>
+      <Text style={styles.label}>Enter Title:</Text>
+      {/* value={title} shows current state, onChangeText passes text
+      to setter */}
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={text => setTitle(text)}
+      />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={text => setContent(text)}
+      />
+      <Button title="Add entry" />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 15,
+    padding: 5,
+    margin: 5
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft: 5
+  }
+});
 
 export default CreateScreen;
