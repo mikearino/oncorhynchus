c3d401eb42c46d4d0a3f7314af9d1f2ab4963b92
Mike Lofgren <mikeylofgren@gmail.com>
2020-02-01 20:30:03 -0800

    create screen boilerplate.

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import { View, Text, Stylesheet } from "react-native";
+import { HeaderTitle } from "react-navigation-stack";
+import { Context } from "../context/EntryContext";
+
+const CreateScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>create</Text>
+    </View>
+  );
+};
+
+export default CreateScreen;
